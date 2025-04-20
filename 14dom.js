f565071a3dee2344a9dedbdffd9100955eb369c5
Deepak Kumar <1172dipu@gmail.com>
2025-04-20 09:43:49 +0530

    update: DOM

---
+++ 14dom.js
@@ -0,0 +1,74 @@
+//Lecture 33 first half
+
+const parent = document.querySelector('.parent')
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[1].innerHTML);
+
+// for (let i = 0; i < parent.children.length; i++) {
+//      console.log(parent.children[i].innerHTML);
+    
+// }
+parent.children[1].style.color = "orange"
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+const dayOne = document.querySelector('.day')
+// console.log(dayOne);
+// console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling);
+
+console.log("NODES: ", parent.childNodes);
+
+//Lecture 33 second half
+
+
+const div = document.createElement('div')
+console.log(div);
+div.className = "main"
+
+div.id = Math.round(Math.random() * 10 + 1)
+
+div.setAttribute("title", "generated title")
+div.style.backgroundColor = "green"
+div.style.padding = "12px"
+
+// div.innerText = "Deepak Kumar "
+const addText = document.createTextNode("Deepak Kumar Learing Javascript")
+
+div.appendChild(addText)
+
+document.body.appendChild(div)
+
+//Lecture 34 
+
+function addLanguage(langName){
+    const li = document.createElement('li');
+    li.innerHTML = `${langName}`
+    document.querySelector('.language').appendChild(li)
+}
+addLanguage("python")
+addLanguage("typescript")
+
+function addOptiLanguage(langName){
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(langName))
+    document.querySelector('.language').appendChild(li)
+}
+addOptiLanguage('golang')
+
+//Edit
+const secondLang = document.querySelector("li:nth-child(2)")
+console.log(secondLang);
+//secondLang.innerHTML = "Mojo"
+const newli = document.createElement('li')
+newli.textContent = "Mojo"
+secondLang.replaceWith(newli)
+
+//edit
+const firstLang = document.querySelector("li:first-child")
+firstLang.outerHTML = '<li>TypeScript</li>'
+
+//remove
+const lastLang = document.querySelector('li:last-child')
+lastLang.remove()
